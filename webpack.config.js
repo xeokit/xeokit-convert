ef33ff4bbd7f314b58a5ed0f66cfaf55cba6579e
xeolabs <lindsay.kay@xeolabs.com>
2022-08-19 03:11:07 +0200

    Webpack tweak

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,11 @@ module.exports = [
             library: {
                 name: 'convert2xkt',
                 type: 'umd',
-            },
+            }
         },
+        // optimization: {
+        //     minimize: false
+        // },
         externalsPresets: {node: true}, // in order to ignore built-in modules like path, fs, etc.
         externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
         module: {
