b77a0795bf4d82778445b823c2df6effd6c74d92
lindsay <lindsay.kay@xeolabs.com>
2024-04-14 22:41:43 +0200

    Rebuild

--- dist/convert2xkt.cjs.js
+++ dist/convert2xkt.cjs.js
@@ -26795,7 +26795,7 @@ function convert2xkt({
         const startTime = new Date();
 
         const sourceConfigs = configs.sourceConfigs || {};
-        const ext = sourceFormat || source.split('.').pop();
+        const ext = sourceFormat || path.extname(source);
         let fileTypeConfigs = sourceConfigs[ext];
 
         if (!fileTypeConfigs) {
