c131f836c6d8180dd5cfb009602a61ef22b13424
lindsay <lindsay.kay@xeolabs.com>
2024-09-06 19:58:42 +0200

    Rebuild

--- dist/xeokit-convert.es.js
+++ dist/xeokit-convert.es.js
@@ -23975,6 +23975,9 @@ function createPrimitiveHash(primitive) {
             hash.push(attributes[key].id);
         }
     }
+    if (primitive.indices) {
+        hash.push(primitive.indices.id);
+    }
     return hash.join(".");
 }
 
